3ca9d2190a2252c40ecb21a4f5a51b8a68937d0b
Em1X-UA <em1x_ua@proton.me>
2023-03-15 21:17:58 +0200

    project started

---
+++ CRM_Project/CRM_Project/local_settings.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-6s(u054(^_oezib*f48jb&u%li!@xtuxnhf=ly3z127%i^@lqe'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
