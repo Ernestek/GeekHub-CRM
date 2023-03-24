d5614aba63843efe62c8e28ec7c1e4f3c60a3de2
Ernestek <iernestek@gmail.com>
2023-03-24 14:38:46 +0200

    fix

--- CRM_Project/account/admin.py
+++ CRM_Project/account/admin.py
@@ -16,7 +16,7 @@ class UserAdmin(_UserAdmin):
         (_('Personal info'), {'fields': ('first_name', 'last_name')}),
         (_('Permissions'), {'fields': ('is_superuser', 'is_active',
                                        'is_staff', 'password_changed',)}),
-        (_('Important dates'), {'fields': ('last_login', )}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined',)}),
     )
 
     add_fieldsets = (
@@ -25,7 +25,7 @@ class UserAdmin(_UserAdmin):
             'fields': ('email',)}
          ),
     )
-    search_fields = ('email', 'first_name', 'last_name')
+    search_fields = ('email', 'first_name', 'last_name', )
     ordering = ('email',)
     filter_horizontal = ()
 
