bb00f24f89dcd177ed4b45dc5b0cf5431564e610
Ernestek <iernestek@gmail.com>
2023-05-01 13:52:26 +0300

    admin fix

--- CRM_Project/projects/serializers/project.py
+++ CRM_Project/projects/serializers/project.py
@@ -31,6 +31,7 @@ class ProjectRetrieveSerializer(serializers.ModelSerializer):
             'status',
             'owner',
             'summary',
+            'created_at',
         )
 
 
--- CRM_Project/tasks/admin.py
+++ CRM_Project/tasks/admin.py
@@ -7,7 +7,7 @@ from tasks.models import UserTask
 @admin.register(UserTask)
 class UserTaskAdmin(admin.ModelAdmin):
 
-    list_display = ('id', 'title', 'user',)
+    list_display = ('id', 'title', 'user', 'created_at')
     list_display_links = ('id', 'title',)
 
     list_filter = ('user', 'project')
@@ -15,6 +15,9 @@ class UserTaskAdmin(admin.ModelAdmin):
         (None, {'fields': ('title', 'text')}),
         (_('User info'), {'fields': ('user', 'user_assigned',)}),
         (_('Task info'), {'fields': ('status', 'project')}),
+        (_('Timestamps'), {'fields': ('created_at', 'updated_at',)})
     )
+    readonly_fields = ('created_at', 'updated_at',)
+
 
 
